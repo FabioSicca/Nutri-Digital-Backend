61c63ad33265f17e4b2c72a20f146eee5c47f8c7
Ezequiel <evilardo@fi.uba.ar>
2025-06-18 23:46:57 -0300

    config: port parametrizable

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ async function bootstrap() {
 		new ErrorInterceptor(),
 		new ResponseInterceptor(),
 	);
-	await app.listen(3000);
+	const port = process.env.PORT || 3000;
+	await app.listen(port);
 }
 bootstrap();
